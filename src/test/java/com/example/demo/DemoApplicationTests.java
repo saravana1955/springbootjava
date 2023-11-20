008ad2f56db7bd0e8c6ad4d444a5d9ceb386183b
saravana1955 <saravana.kumar@provility.com>
2023-11-20 16:21:20 +0530

    initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		assertEquals(40, 10 + 20);
+	}
+
+}
+
+
+
